77c793a168aff3e596c3d17931b85677062b7fe9
Macdaveed <Macdaveedgolden@gmail.com>
2024-01-19 14:34:50 +0100

    7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[3:]
+word_first_3 = word[:3]
 word_last_2: str = word[-2:]
 middle_word: str = word[1:-1]
 print(f"First 3 letters: {word_first_3}")
