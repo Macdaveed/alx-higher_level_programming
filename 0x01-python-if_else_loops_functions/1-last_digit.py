73c4c54636506e0ed8d062e30e9c6e670bdbaf3e
Macdaveed <Macdaveedgolden@gmail.com>
2024-02-01 13:24:07 +0100

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,11 @@ sign = '-' if new < 0 else ''
 literal = "and is than 6 and not 0"
 new_literal = literal
 if last_num > 5:
-    print(f"last digit of {new} is {sign}{last_num} and is greater than 5")
+    last_num == 0
+    print(f"Last digit of {new} is {sign}{last_num} and is greater than 5")
+elif last_num == 0:
+    print(f"Last digit of {new} is {last_num} and is 0")
 elif last_num < 6:
-    print(f"last digit of {new} is {sign}{last_num} and is less than 6 not 0")
+    print(f"Last digit of {new} is {sign}{last_num} and is less than 6 not 0")
 else:
-    print(f"last_digit of {new} is {sign}{last_num} and is 0")
+    print()
